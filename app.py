dc5fe0c532d5fada31128de357b7213b7f961317
Yazmin Rodriguez <79778345+Yazz-01@users.noreply.github.com>
2021-05-27 11:03:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,59 +4,48 @@ from flask import Flask
 # Create an app, being sure to pass __name__
 app = Flask(__name__)
 
-# 1. Home page.
+# 1. Home page route.
 @app.route("/")
 def home():
     print("Server received request from 'Home' page...")
     return "Welcome to my 'Home'page!"
 
 
-# 2. List all routes that are available.
+# 2. Index listing all routes that are available.
 @app.route("/about")
-def about():
-    print("Server received request from 'About' page...")
-    return "Welcome to my 'About' page!"
-
-/api/v1.0/precipitation
+def index():
+    print("Server received request from 'Index' page...")
+    return "Available Routes:"
 
 
-Convert the query results to a dictionary using date as the key and prcp as the value.
+# 3. Precipitation
+@app.route("/api/v1.0/precipitation")
+def show_precipitation():
+    return jsonify()
+
+# 4. Stations
+@app.route("/api/v1.0/stations")
+def show_stations():
+    return jsonify()
 
 
-Return the JSON representation of your dictionary.
+# 5. Dates and temperature observations 
+# of the most active station for the last year of data.
+@app.route("/api/v1.0/tobs")
+def show_stations():
+    return jsonify()
 
-
-
-
-/api/v1.0/stations
-
-Return a JSON list of stations from the dataset.
-
-
-
-/api/v1.0/tobs
-
-
-Query the dates and temperature observations of the most active station for the last year of data.
-
-
-Return a JSON list of temperature observations (TOBS) for the previous year.
-
-
-
-
-/api/v1.0/<start> and /api/v1.0/<start>/<end>
-
-
-Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
-
-
-When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
-
-
-When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
+# Return a JSON list of the minimum temperature, 
+# the average temperature, and the max temperature 
+# for a given start or start-end range.
+@app.route("/api/v1.0/<start> and /api/v1.0/<start>/<end>")
+def show_stations():
+    return jsonify()
 
 
 
 
 
+if __name__== "__main__":
+    app.run
+    
